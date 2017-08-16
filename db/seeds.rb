857ed460b440bed30b0571f6dae904974fb51cb3
Johannes Vaisfeld <jvaisfeld@gmail.com>
2017-08-16 12:06:08 +0200

    routes added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    category:     "chinese",
+    phone_number: "12345"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    category:     "italian",
+    phone_number: "123456"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    category:     "japanese",
+    phone_number: "12345678"
+  },
+  {
+    name:         "Max und Moritz",
+    address:      "Oranienstr",
+    category:     "belgian",
+    phone_number: "123456789"
+  },
+  {
+    name:         "le baguette",
+    address:      "paris street",
+    category:     "french",
+    phone_number: "12345678910"
+  },
+]
+Restaurant.create!(restaurants_attributes)
